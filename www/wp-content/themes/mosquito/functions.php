bd9c84c62afc08ab0673f2ec808a09e094b38f38
Jason Gegere <jason@hgmail.com>
2011-10-06 12:35:04 -0500

    hash bbq, flickr

--- www/wp-content/themes/mosquito/functions.php
+++ www/wp-content/themes/mosquito/functions.php
@@ -1,21 +1,18 @@
 <?php
 
-
 /**
-* Navigation menus
-*
-* @since HTMLgraphic
-*/
+ * Navigation menus
+ *
+ * @since HTMLgraphic
+ */
 function register_my_menus() {
-	register_nav_menus(
-		array(
-				'primary-menu' => __( 'Primary Menu' ),
-				'home-menu' => __( 'Home Menu' )
-		)
-	);
+  register_nav_menus(
+          array(
+              'primary-menu' => __('Primary Menu'),
+              'home-menu' => __('Home Menu')
+          )
+  );
 }
-add_action( 'init', 'register_my_menus' ); 
-
 
 /**
  * Display breadcrumbs
@@ -77,20 +74,31 @@ function the_breadcrumb() {
   echo '</ul>';
 }
 
-
-
 /**
-* Use a cache jQuery script snippet to improve load performance
-*
-* @since HTMLgraphic
-*/
-function load_better_jquery_script() {
+ * Use a cache jQuery script snippet to improve load performance
+ *
+ * @since HTMLgraphic
+ */
+function load_jquery_script_cdn() {
   wp_deregister_script('jquery');
   echo "<script src='//ajax.googleapis.com/ajax/libs/jquery/1.6.0/jquery.min.js'></script>";
   echo "<script>!window.jQuery && document.write(unescape('%3Cscript src=\"/wp-includes/js/jquery/jquery.js\"%3E%3C/script%3E'))</script>";
 }
-add_action('wp_head', 'load_better_jquery_script', 1);
 
+/**
+ * Cache the BBQ script (Back Button & Query Library) for dealing with hashes in the URL
+ *
+ * @since HTMLgraphic
+ */
+function load_bbq_jquery_script() {
+  wp_register_script('jquery-ba-bbq', get_stylesheet_directory_uri() . '/js/jquery.ba-bbq.min.js');
+  wp_enqueue_script('jquery-ba-bbq');
+}
 
-
+//
+// action bindings
+//
+add_action('init', 'register_my_menus');
+add_action('wp_enqueue_scripts', 'load_jquery_script_cdn', 1);
+add_action('wp_enqueue_scripts', 'load_bbq_jquery_script');
 ?>
---
+++ www/wp-content/themes/mosquito/page-90.php
@@ -0,0 +1,15 @@
+<?php get_header(); ?>
+
+<section>
+  <article id="post-<?php the_ID(); ?>">
+    <header>
+      <h1><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h1>
+      <?php /* ?><p>Posted on <?php the_time('F jS, Y'); ?> by <?php the_author(); ?></p><?php */ ?>
+      <?php the_breadcrumb(); ?>
+    </header>
+  </article>
+</section>
+
+[flickr-gallery]
+
+<?php get_footer(); ?>
