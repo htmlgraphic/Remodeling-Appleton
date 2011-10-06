c31ed8218981771830f1886ad943f58d9bf03292
Jason Gegere <jason@hgmail.com>
2011-10-06 02:43:16 -0500

    Unstyled layout created

--- www/wp-content/themes/mosquito/functions.php
+++ www/wp-content/themes/mosquito/functions.php
@@ -1,5 +1,22 @@
 <?php
 
+
+/**
+* Navigation menus
+*
+* @since HTMLgraphic
+*/
+function register_my_menus() {
+	register_nav_menus(
+		array(
+				'primary-menu' => __( 'Primary Menu' ),
+				'home-menu' => __( 'Home Menu' )
+		)
+	);
+}
+add_action( 'init', 'register_my_menus' ); 
+
+
 /**
  * Display breadcrumbs
  *
@@ -60,19 +77,20 @@ function the_breadcrumb() {
   echo '</ul>';
 }
 
-//
-// action binding functions
-//
 
+
+/**
+* Use a cache jQuery script snippet to improve load performance
+*
+* @since HTMLgraphic
+*/
 function load_better_jquery_script() {
   wp_deregister_script('jquery');
   echo "<script src='//ajax.googleapis.com/ajax/libs/jquery/1.6.0/jquery.min.js'></script>";
   echo "<script>!window.jQuery && document.write(unescape('%3Cscript src=\"/wp-includes/js/jquery/jquery.js\"%3E%3C/script%3E'))</script>";
 }
-
-//
-// action bindings
-//
-
 add_action('wp_head', 'load_better_jquery_script', 1);
+
+
+
 ?>
--- www/wp-content/themes/mosquito/page-about.php
+++ www/wp-content/themes/mosquito/page-about.php
@@ -1,58 +1,52 @@
 <?php get_header(); ?>
-
-<div id="page">
-  <div id="title"><?php the_title(); ?></div>
-
-  <?php the_breadcrumb(); ?>
-
-  <script type="text/javascript">
-    $(function() {
-      $('#about-us-navigation-menu ul li a').click(function(event) {
-        event.preventDefault();
-        $('.about-subpage').hide();
-        $('#about-subpage-' + $(this).attr('rel')).show();
-      });
-    });
-  </script>
-
-  <div id="about-us-navigation-menu" style="float:left;">
-    <ul>
-      <li><a href="#history" rel="history">History</a></li>
-      <li><a href="#awards" rel="awards">Awards</a></li>
-      <li><a href="#accreditation" rel="accreditation">Accreditation</a></li>
-      <li><a href="#certificates" rel="certificates">Certificates</a></li>
-      <li><a href="#subcontractors" rel="subcontractors">Subcontractors</a></li>
-      <li><a href="#testimonials" rel="testimonials">Testimonials</a></li>
-      <li><a href="#contact" rel="contact">Contact</a></li>
-    </ul>
-  </div>
-
-  <?php
-  $about_page_ids = array(
-      18 => 'history',
-      20 => 'awards',
-      22 => 'accreditation',
-      24 => 'certificates',
-      26 => 'subcontractors',
-      28 => 'testimonials',
-      30 => 'contact'
-  );
-  ?>
-
-  <?php foreach ($about_page_ids as $about_page_id => $about_page_name): ?>
-    <?php
-    $page_data = get_page($about_page_id);
-    ?>
-    <div id="about-subpage-<?php echo $about_page_name; ?>" class="about-subpage" style="display:none;float:left;">
-      <?php
-      $content = apply_filters('the_content', $page_data->post_content);
-      $title = $page_data->post_title;
-      ?>
-      <?php echo $content; ?>
-    </div>
-    <?php
-    ?>
-  <?php endforeach; ?>
-</div>
+  
+		<section>
+			<article id="post-<?php the_ID(); ?>">
+				<header>
+					<h1><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h1>
+					<?php /*?><p>Posted on <?php the_time('F jS, Y'); ?> by <?php the_author(); ?></p><?php */?>
+                    <?php the_breadcrumb(); ?>                    
+				</header>
+				<section>
+					<div id="about-us-navigation-menu" style="float:left;">
+                      <ul>
+                        <li><a href="#history" rel="history">History</a></li>
+                        <li><a href="#awards" rel="awards">Awards</a></li>
+                        <li><a href="#accreditation" rel="accreditation">Accreditation</a></li>
+                        <li><a href="#certificates" rel="certificates">Certificates</a></li>
+                        <li><a href="#subcontractors" rel="subcontractors">Subcontractors</a></li>
+                        <li><a href="#testimonials" rel="testimonials">Testimonials</a></li>
+                        <li><a href="#contact" rel="contact">Contact</a></li>
+                      </ul>
+                    </div>
+					<?php
+                    $about_page_ids = array(
+                        18 => 'history',
+                        20 => 'awards',
+                        22 => 'accreditation',
+                        24 => 'certificates',
+                        26 => 'subcontractors',
+                        28 => 'testimonials',
+                        30 => 'contact'
+                    );
+                    ?>
+                  
+                    <?php foreach ($about_page_ids as $about_page_id => $about_page_name): ?>
+                      <?php
+                      $page_data = get_page($about_page_id);
+                      ?>
+                      <div id="about-subpage-<?php echo $about_page_name; ?>" class="about-subpage" style="display:none;float:left;">
+                        <?php
+                        $content = apply_filters('the_content', $page_data->post_content);
+                        $title = $page_data->post_title;
+                        ?>
+                        <?php echo $content; ?>
+                      </div>
+                      <?php
+                      ?>
+                    <?php endforeach; ?>
+				</section>
+			</article>
+		</section>
 
 <?php get_footer(); ?>
