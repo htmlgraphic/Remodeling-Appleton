73269abfe91e956f92c119df4cb7236f3cc183d1
Tom Farvour <tom@farvour.com>
2011-10-07 13:56:58 -0500

    fix bbq script ref

--- www/wp-content/themes/mosquito/functions.php
+++ www/wp-content/themes/mosquito/functions.php
@@ -14,24 +14,22 @@ function register_my_menus() {
   );
 }
 
-
 /**
-* Bread crumbs
-*
-* @since HTMLgraphic
-*/
-function hg_bread_crumbs() {  
-	do_action('hg_bread_crumbs');  
-}  
-
+ * Bread crumbs
+ *
+ * @since HTMLgraphic
+ */
+function hg_bread_crumbs() {
+  do_action('hg_bread_crumbs');
+}
 
 function load_yoast_breadcrumb() {
-  if ( function_exists('yoast_breadcrumb') ) {
-	  echo yoast_breadcrumb("","",false);
+  if (function_exists('yoast_breadcrumb')) {
+    echo yoast_breadcrumb("", "", false);
   }
 }
-add_action( 'hg_bread_crumbs', 'load_yoast_breadcrumb' ); 
 
+add_action('hg_bread_crumbs', 'load_yoast_breadcrumb');
 
 /**
  * Use a cache jQuery script snippet to improve load performance
@@ -44,7 +42,10 @@ function load_better_jquery_script() {
   echo "<script>!window.jQuery && document.write(unescape('%3Cscript src=\"/wp-includes/js/jquery/jquery.js\"%3E%3C/script%3E'))</script>";
 }
 
-add_action('wp_head', 'load_better_jquery_script', 1);
+function load_bbq_jquery_script() {
+  wp_register_script('jquery-bbq', get_stylesheet_directory_uri() . '/js/jquery.ba-bbq.min.js');
+  wp_enqueue_script('jquery-bbq');
+}
 
 function load_scrollTo_jquery_script() {
   wp_register_script('jquery-scrollTo', get_stylesheet_directory_uri() . '/js/jquery.scrollTo-1.3.3.min.js');
@@ -65,7 +66,7 @@ function load_serialScroll_jquery_script() {
 // action bindings
 //
 add_action('init', 'register_my_menus');
-add_action('wp_enqueue_scripts', 'load_jquery_script_cdn', 1);
+add_action('wp_head', 'load_better_jquery_script', 1);
 add_action('wp_enqueue_scripts', 'load_bbq_jquery_script');
 add_action('wp_enqueue_scripts', 'load_scrollTo_jquery_script');
 add_action('wp_enqueue_scripts', 'load_localscroll_jquery_script');
