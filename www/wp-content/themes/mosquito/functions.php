1de8d425e2f27f350ec8ef4cfb4c0333fd7aaf1c
Tom Farvour <tom@farvour.com>
2011-10-07 13:01:11 -0500

    slider on interior kitchen rennovation gallery

--- www/wp-content/themes/mosquito/functions.php
+++ www/wp-content/themes/mosquito/functions.php
@@ -95,10 +95,28 @@ function load_bbq_jquery_script() {
   wp_enqueue_script('jquery-ba-bbq');
 }
 
+function load_scrollTo_jquery_script() {
+  wp_register_script('jquery-scrollTo', get_stylesheet_directory_uri() . '/js/jquery.scrollTo-1.3.3.min.js');
+  wp_enqueue_script('jquery-scrollTo');
+}
+
+function load_localscroll_jquery_script() {
+  wp_register_script('jquery-localscroll', get_stylesheet_directory_uri() . '/js/jquery.localscroll-1.2.6-min.js');
+  wp_enqueue_script('jquery-localscroll');
+}
+
+function load_serialScroll_jquery_script() {
+  wp_register_script('jquery-serialScroll', get_stylesheet_directory_uri() . '/js/jquery.serialScroll-1.2.1.min.js');
+  wp_enqueue_script('jquery-serialScroll');
+}
+
 //
 // action bindings
 //
 add_action('init', 'register_my_menus');
 add_action('wp_enqueue_scripts', 'load_jquery_script_cdn', 1);
 add_action('wp_enqueue_scripts', 'load_bbq_jquery_script');
+add_action('wp_enqueue_scripts', 'load_scrollTo_jquery_script');
+add_action('wp_enqueue_scripts', 'load_localscroll_jquery_script');
+add_action('wp_enqueue_scripts', 'load_serialScroll_jquery_script');
 ?>
